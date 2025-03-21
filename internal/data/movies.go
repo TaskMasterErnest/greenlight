ec3a185fbf03e0a1e9774503f236071b6a39dcc5
TaskMasterErnest <kluernest08@gmail.com>
2025-03-21 14:19:38 +0000

    included struct tags for json fields

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,11 +3,11 @@ package data
 import "time"
 
 type Movie struct {
-	ID        int64
-	CreatedAt time.Time
-	Title     string
-	Year      int32
-	Runtime   int32
-	Genres    []string
-	Version   int32
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"` // this field not relevant to users
+	Title     string    `json:"title,omitempty"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres"`
+	Version   int32     `json:"version"`
 }
