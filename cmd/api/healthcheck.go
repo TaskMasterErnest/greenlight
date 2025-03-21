a939518e01808ec24e42a13ee2f9397ad1a8578e
TaskMasterErnest <kluernest08@gmail.com>
2025-03-21 11:07:55 +0000

    fixed format response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,12 @@ import (
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Status: available")
-	fmt.Fprintf(w, "Environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "Version: %s\n", version)
+	// inscribing a json text raw
+	js := `{"status": "available", "Environment": %q, "Version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	// setting header to recognize and parse json
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(js))
 }
