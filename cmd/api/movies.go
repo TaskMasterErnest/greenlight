45aa4176facc4d14ad86e4527241b4bbcddd1504
TaskMasterErnest <kluernest08@gmail.com>
2025-03-21 13:34:04 +0000

    modded showMovieHandler with custom data

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/TaskMasterErnest/greenlight/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,5 +21,21 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "Showing details of movie %d\n", id)
+	// initialize a Movie struct instance
+	//using ID from context and some dummy data
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	// encode movie data and send as JSON response
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not parse your movie request", http.StatusInternalServerError)
+	}
 }
