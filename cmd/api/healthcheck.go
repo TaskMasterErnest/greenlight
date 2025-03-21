7531b7f5c984dece26ceff83d3513c516ea1c8b4
TaskMasterErnest <kluernest08@gmail.com>
2025-03-21 13:04:21 +0000

    converting to json without helper

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	// inscribing a json text raw
-	js := `{"status": "available", "Environment": %q, "Version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	// stating the data in a map object type
+	data := map[string]string{
+		"Status":      "Available",
+		"Environment": app.config.env,
+		"Version":     version,
+	}
+
+	// marshal the data struct into JSON
+	js, err := json.Marshal(data)
+	if err != nil {
+		// this uses the error logger to log the err from marshalling the json
+		app.logger.Error(err.Error())
+		http.Error(w, "Error processing JSON marshalling", http.StatusInternalServerError)
+		return
+	}
+
+	// a nicety to make this viewable in terminals
+	js = append(js, '\n')
 
 	// setting header to recognize and parse json
 	w.Header().Set("Content-Type", "application/json")
