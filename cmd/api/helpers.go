332a6762372c1a2c165efb0be2c45f253bf992b1
TaskMasterErnest <kluernest08@gmail.com>
2025-03-24 16:13:10 +0000

    added readJSON helper to read JSON input and also triage errors, returning plain-english messages

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -48,6 +50,52 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write([]byte(js))
+
+	return nil
+}
+
+// a readJSON helper function to help with reading JSON input
+// we use this to also triage errors regarding JSON input adn provide a suitable error message
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dest any) error {
+	// decode request into the target destination
+	err := json.NewDecoder(r.Body).Decode(dest)
+	if err != nil {
+		// triage errors, using common;y gotten errors
+		var syntaxError *json.SyntaxError
+		var unMarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+		// check whether the error has the type json.SyntaxError
+		// return a plain-English error message which includes the location of the problem
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
+			// in some circumstances, it may return an io.ErrUnexpectedEOF for syntax errors in the JSON
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+
+			// catch any UnmarshalTypeError when the JSON type is wrong for the target destination
+		case errors.As(err, unMarshalTypeError):
+			if unMarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unMarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unMarshalTypeError.Offset)
+
+			// check if the request body is empty, this gives a io.EOF error
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+			// throw an InvalidUnmarshalError if we pass something that is not a non-nil pointer to Decode()
+			// catch this and panic instead of returning an error to the Handler
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+
+			// for anything else, return the error message as is
+		default:
+			return err
+		}
+	}
 
 	return nil
 }
