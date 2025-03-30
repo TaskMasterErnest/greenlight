01e89998c9b8b7017c116dc97ecc45e9c1956317
TaskMasterErnest <kluernest08@gmail.com>
2025-03-30 09:20:17 +0000

    validating movie input JSON

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/TaskMasterErnest/greenlight/internal/data"
+	"github.com/TaskMasterErnest/greenlight/internal/validator"
 )
 
 // showMovieHandler
@@ -50,6 +51,39 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// initialize a new Validator instance for input Validation
+	v := validator.New()
+
+	// use the Check method from the validator to execute validation checks
+	// this will add errors to the errors map if the validations do not evaluate to true
+	// <validating Title input>
+	v.Check(input.Title != "", "title", "must be provided")
+	v.Check(len(input.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	// <validating Year input>
+	v.Check(input.Year != 0, "year", "must be provided")
+	v.Check(input.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	// <validating Runtime input>
+	v.Check(input.Runtime != 0, "runtime", "must be provided")
+	v.Check(input.Runtime > 0, "runtime", "must be a positive integer")
+
+	// <validating Genre input>
+	v.Check(input.Genres != nil, "genres", "must be provided")
+	v.Check(len(input.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(input.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	// now we check if all the genres are unique
+	v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
+
+	// check if any of the checks failed with the Valid() method.
+	// if affirmative, then use the failedValidationResponse helper to send a response to the client
+	// pass in the v.Errors map, map of errors accumulated
+	if !v.Valid() {
+		app.FailedValidationResponse(w, r, v.Errors)
 		return
 	}
 
