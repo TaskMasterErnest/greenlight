66a71f4d8e0bd75928fe7dd09d1ed49fbe85a702
TaskMasterErnest <kluernest08@gmail.com>
2025-04-01 04:36:39 +0000

    CRUD functions

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/TaskMasterErnest/greenlight/internal/validator"
@@ -40,4 +41,30 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
 	// now we check if all the genres are unique
 	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
+// methods for performing CRUD to Movies
+// a MovieModel struct that wraps an sql.DB connection pool
+type MovieModel struct {
+	DB *sql.DB
+}
+
+// insert a movie record into the Movie table
+func (m MovieModel) Insert(movie *Movie) error {
+	return nil
+}
+
+// fetching a movie record from the Movie table
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+// update a specific movie record in the Movie table
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+// delete a specific record from the Movies table
+func (m MovieModel) Delete(id int64) error {
+	return nil
 }
