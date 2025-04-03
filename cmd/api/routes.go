7d49220101bca0bc3be3fdcf6bbcc833c648ad17
TaskMasterErnest <kluernest08@gmail.com>
2025-04-03 15:17:29 +0000

    added route for movie update logic

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -18,6 +18,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/healthz", app.healthCheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
 
 	// wrap the call to router with the recoverPanic middleware
 	return app.recoverPanic(router)
