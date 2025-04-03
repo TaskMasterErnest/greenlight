2d538f0f96c63184194b30a348b4da7ff90da867
TaskMasterErnest <kluernest08@gmail.com>
2025-04-03 13:43:34 +0000

    added logic for get method with error handling

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/TaskMasterErnest/greenlight/internal/data"
 	"github.com/TaskMasterErnest/greenlight/internal/validator"
@@ -18,15 +18,16 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		app.notFoundResponse(w, r)
 	}
 
-	// initialize a Movie struct instance
-	//using ID from context and some dummy data
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	// call the Get() method to fetch specific movie data, return errors
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	// envelope the movie in the envelope type
